7594b8f1a9e8d12e87804a5693e04add6a4c5cd6
Matei Nicolae <matei.nick@gmail.com>
2015-05-09 11:08:53 +0300

    move selenium web driver elements/actions into LoginPage

--- src/main/java/org/fasttrackit/workshop/pagefactory/login/LoginPage.java
+++ src/main/java/org/fasttrackit/workshop/pagefactory/login/LoginPage.java
@@ -1,5 +1,7 @@
 package org.fasttrackit.workshop.pagefactory.login;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
@@ -9,10 +11,42 @@ import org.slf4j.LoggerFactory;
 public class LoginPage {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginPage.class);
 
+    // TODO remove
+    public static WebDriver driver;
+
     @FindBy(how = How.ID, using = "email")
     private WebElement emailEl;
 
-    public void enterEmail(String email) {
+    public void enterEmail2(String email) {
         emailEl.sendKeys(email);
+    }
+
+
+    public String getErrorMessage() {
+        WebElement errorMsg = driver.findElement(By.className("error-msg"));
+        return errorMsg.getText();
+    }
+
+    public void login(String email, String passValue) throws Throwable {
+        enterEmail(email);
+
+        System.out.println("set pass");
+        WebElement pass = driver.findElement(By.xpath("//form/div[2]//input"));
+        pass.sendKeys(passValue);
+
+        System.out.println("click on login");
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+        loginButton.click();
+    }
+
+    public void enterEmail(String email) {
+        System.out.println("set email" + email);
+        WebElement emailField = driver.findElement(By.id("email"));
+        emailField.sendKeys(email);
+    }
+
+    public void doLogout() {
+        WebElement logoutButton = driver.findElement(By.xpath("//nav//a[normalize-space(text())='Logout']"));
+        logoutButton.click();
     }
 }
--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -7,6 +7,7 @@ import org.fasttrackit.util.TestBaseNative;
 import org.fasttrackit.workshop.pagefactory.login.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,72 +19,42 @@ public class LoginSteps extends TestBaseNative {
 
     LoginPage loginPage;
 
-    @Given("^I open login page$")
-    public void I_open_login_page() {
-        driver.get("file:///E:/Aplication/Testy/src/test/functional/login.html");
-
-        //loginPage = PageFactory.initElements(driver, LoginPage.class);
-    }
-
     @When("^I enter email \"([^\"]*)\"$")
     public void I_enter_email(String email) {
-        enterEmail(email);
+        loginPage.enterEmail(email);
     }
 
 
     @Given("^I open app$")
     public void i_open_app() throws Throwable {
         driver.get("file:///C:/Cloud/Dropbox/Public/FastTrackIT/app-demo/login.html");
+
+        loginPage = PageFactory.initElements(driver, LoginPage.class);
+
+        // TODO remove (shortcut to driver)
+        LoginPage.driver = driver;
     }
 
     @When("^login into the app$")
     public void login_into_the_app() throws Throwable {
-        login("eu@fast.com", "eu.pass");
+        loginPage.login("eu@fast.com", "eu.pass");
     }
 
     @Then("^I'm logged in$")
     public void i_m_logged_in() throws Throwable {
-        doLogout();
+        loginPage.doLogout();
     }
 
     @When("^I insert invalid credentials$")
     public void i_insert_invalid_credentials() throws Throwable {
-        login("noi@fast.com", "noi.pass");
+        loginPage.login("noi@fast.com", "noi.pass");
     }
 
     @Then("^I won't be logged in$")
     public void i_won_t_be_logged_in() throws Throwable {
-        String message = getErrorMessage();
+        String message = loginPage.getErrorMessage();
 
         System.out.println("error: " + message);
         assertThat(message, is("Invalid user or password!"));
-    }
-
-    public String getErrorMessage() {
-        WebElement errorMsg = driver.findElement(By.className("error-msg"));
-        return errorMsg.getText();
-    }
-
-    public void login(String email, String passValue) throws Throwable {
-        enterEmail(email);
-
-        System.out.println("set pass");
-        WebElement pass = driver.findElement(By.xpath("//form/div[2]//input"));
-        pass.sendKeys(passValue);
-
-        System.out.println("click on login");
-        WebElement loginButton = driver.findElement(By.tagName("button"));
-        loginButton.click();
-    }
-
-    public void enterEmail(String email) {
-        System.out.println("set email" + email);
-        WebElement emailField = driver.findElement(By.id("email"));
-        emailField.sendKeys(email);
-    }
-
-    public void doLogout() {
-        WebElement logoutButton = driver.findElement(By.xpath("//nav//a[normalize-space(text())='Logout']"));
-        logoutButton.click();
     }
 }
