1e11037c0b1a683af2897446c9987afe78b90ca3
Matei Nicolae <matei.nick@gmail.com>
2015-07-04 11:34:52 +0300

    create loginPage.enterCredentials method

--- src/main/java/org/fasttrackit/workshop/pagefactory/login/LoginPage.java
+++ src/main/java/org/fasttrackit/workshop/pagefactory/login/LoginPage.java
@@ -14,7 +14,18 @@ public class LoginPage {
     @FindBy(how = How.ID, using = "loginButton")
     private WebElement loginButton;
 
+    @FindBy(how = How.ID, using = "email")
+    private WebElement email;
+
+    @FindBy(how = How.ID, using = "password")
+    private WebElement password;
+
     public void clickOnLoginButton() {
         loginButton.click();
+    }
+
+    public void enterCredentials(String emailValue, String passValue) {
+        email.sendKeys(emailValue);
+        password.sendKeys(passValue);
     }
 }
--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -73,11 +73,7 @@ public class LoginSteps extends TestBaseNative {
 
     @When("^I enter \"([^\"]*)\"/\"([^\"]*)\" credentials$")
     public void I_enter_credentials(String emailValue, String passValue) {
-        WebElement email = driver.findElement(By.id("email"));
-        email.sendKeys(emailValue);
-
-        WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys(passValue);
+        loginPage.enterCredentials(emailValue, passValue);
     }
 
     @Then("^I expect \"([^\"]*)\" error message$")
