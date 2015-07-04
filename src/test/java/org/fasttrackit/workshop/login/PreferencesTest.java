3cd09a76e3e90b4e7e2b51bef325d873d5ac4435
Matei Nicolae <matei.nick@gmail.com>
2015-07-04 20:21:17 +0300

    add PreferencesTest

---
+++ src/test/java/org/fasttrackit/workshop/login/PreferencesTest.java
@@ -0,0 +1,18 @@
+package org.fasttrackit.workshop.login;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber", "json:target/jsonReports/LoginTest.json"},
+        glue = {
+                "org.fasttrackit.workshop.login"
+        },
+        features = {
+                "src/test/resources/feature/workshop/change-password.feature"
+        }
+)
+@RunWith(Cucumber.class)
+public class PreferencesTest {
+}
