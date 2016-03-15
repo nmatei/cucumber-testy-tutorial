14531dfcf7f6fb7cdf33b5164711b28c44dc813c
Matei Nicolae <matei.nick@gmail.com>
2016-03-15 23:28:55 +0200

    add test: successChangePassword (while learning xpath & css selectors)

--- src/test/java/org/fasttrackit/onlinelibrary/login/FistLoginTest.java
+++ src/test/java/org/fasttrackit/onlinelibrary/login/FistLoginTest.java
@@ -1,5 +1,6 @@
 package org.fasttrackit.onlinelibrary.login;
 
+import com.sdl.selenium.web.utils.Utils;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -51,6 +52,35 @@ public class FistLoginTest extends TestBase {
         openLoginPage();
         doLogin("", "");
         assertThatErrorIs("Please enter your email!");
+    }
+
+    @Test
+    public void successChangePassword() {
+        openLoginPage();
+        doLogin("eu@fast.com", "eu.pass");
+
+        WebElement preferencesButton = driver.findElement(By.xpath("//nav//button"));
+        preferencesButton.click();
+
+        // because of window animation (slide down)
+        Utils.sleep(300);
+
+        // css selector: $$("#preferences-win input[name=password]")
+        WebElement currentPassField = driver.findElement(By.xpath("//div[@id='preferences-win']//input[@name='password']"));
+        currentPassField.sendKeys("eu.pass");
+
+        WebElement newPassField = driver.findElement(By.xpath("//input[@name='newPassword']"));
+        newPassField.sendKeys("eu.pass2");
+
+        WebElement repeatPassField = driver.findElement(By.xpath("//input[@name='newPasswordRepeat']"));
+        repeatPassField.sendKeys("eu.pass2");
+
+        WebElement saveBtn = driver.findElement(By.cssSelector("#preferences-win button.btn-warning"));
+        saveBtn.click();
+
+        WebElement statusElement = driver.findElement(By.cssSelector("#preferences-win .status-msg"));
+        System.out.println(statusElement.getText());
+        assertThat(statusElement.getText(), is("Your password has been successfully changed."));
     }
 
     private void assertThatErrorIs(String message) {
