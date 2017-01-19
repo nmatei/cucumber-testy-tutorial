da8849dcaedbe013d831fbb0bc95c158774379b1
Matei Nicolae <matei.nick@gmail.com>
2017-01-19 20:46:45 +0200

    invalid login test

--- src/test/java/org/fasttrackit/automation/LoginTest.java
+++ src/test/java/org/fasttrackit/automation/LoginTest.java
@@ -7,20 +7,16 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class LoginTest extends TestBase {
 
     @Test
     public void validLoginTest() {
-        System.out.println("ready");
-        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+        openBrowser();
 
-        WebElement emailField = driver.findElement(By.id("email"));
-        WebElement passField = driver.findElement(By.name("password"));
-        WebElement loginBtn = driver.findElement(By.className("login-btn"));
-
-        emailField.sendKeys("eu@fast.com");
-        passField.sendKeys("eu.pass111");
-        loginBtn.click();
+        login("eu@fast.com", "eu.pass");
 
         try {
             WebElement logoutBtn = driver.findElement(By.linkText("Logout"));
@@ -28,5 +24,34 @@ public class LoginTest extends TestBase {
         } catch (NoSuchElementException er) {
             Assert.fail("Could not login. Logout button was not present!");
         }
+    }
+
+    @Test
+    public void invalidPasswordTest() {
+        openBrowser();
+
+        login("eu@fast.com", "eu.pass123");
+
+        WebElement errorElement = driver.findElement(By.className("error-msg"));
+        System.out.println(errorElement.getText());
+        //Assert.assertEquals(errorElement.getText(), "Invalid user or password!");
+        assertThat(errorElement.getText(), is("Invalid user or password!"));
+    }
+
+    private void openBrowser() {
+        System.out.println("ready");
+        //driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+        driver.get("file:///C:/Producs/Testy/src/test/functional/app-demo/login.html");
+        System.out.println("------------------");
+    }
+
+    public void login(String user, String pass) {
+        WebElement emailField = driver.findElement(By.id("email"));
+        WebElement passField = driver.findElement(By.name("password"));
+        WebElement loginBtn = driver.findElement(By.className("login-btn"));
+
+        emailField.sendKeys(user);
+        passField.sendKeys(pass);
+        loginBtn.click();
     }
 }
