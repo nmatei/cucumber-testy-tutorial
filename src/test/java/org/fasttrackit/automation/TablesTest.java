40b20508c89b92994f3956eaa3c2582fdc697786
Matei Nicolae <matei.nick@gmail.com>
2017-02-16 20:39:48 +0200

    use TableRows as container for TableCell

--- src/test/java/org/fasttrackit/automation/TablesTest.java
+++ src/test/java/org/fasttrackit/automation/TablesTest.java
@@ -1,9 +1,11 @@
 package org.fasttrackit.automation;
 
 import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.form.CheckBox;
+import com.sdl.selenium.web.table.Cell;
+import com.sdl.selenium.web.table.Row;
+import com.sdl.selenium.web.table.Table;
 import org.fasttrackit.util.TestBase;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class TablesTest extends TestBase {
@@ -16,6 +18,19 @@ public class TablesTest extends TestBase {
         //WebElement checkbox = driver.findElement(By.xpath("//table//input"));
         WebLocator checkbox = new WebLocator().setElPath("//table//input");
 
+        checkbox.click();
+    }
+
+    Table table = new Table();
+
+    @Test
+    public void selectRowByEmail() {
+        openLoginPage();
+        loginView.login("eu@fast.com", "eu.pass");
+
+        Row row = table.getRow(new Cell(4, "davidmiller@mail.com"));
+
+        CheckBox checkbox = new CheckBox(row);
         checkbox.click();
     }
 }
