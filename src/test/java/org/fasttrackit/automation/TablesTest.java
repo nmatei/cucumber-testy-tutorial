b61916c94b9c32cc9c9e491c212819e3dd646bdf
Matei Nicolae <matei.nick@gmail.com>
2017-02-16 21:32:37 +0200

    add dataprovider for row selection

--- src/test/java/org/fasttrackit/automation/TablesTest.java
+++ src/test/java/org/fasttrackit/automation/TablesTest.java
@@ -6,6 +6,7 @@ import com.sdl.selenium.web.table.Cell;
 import com.sdl.selenium.web.table.Row;
 import com.sdl.selenium.web.table.Table;
 import org.fasttrackit.util.TestBase;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TablesTest extends TestBase {
@@ -39,5 +40,27 @@ public class TablesTest extends TestBase {
         WebLocator rowLocator = new WebLocator(tableLocator).setTag("tr").setChildNodes(firstNameLocator, lastNameLocator);
         WebLocator checkboxLocator = new WebLocator().setContainer(rowLocator).setTag("input");
         checkboxLocator.click();
+    }
+
+    @Test
+    public void selectRowByEmailsSetup(){
+        openLoginPage();
+        loginView.login("eu@fast.com", "eu.pass");
+    }
+
+    @Test(dependsOnMethods = "selectRowByEmailsSetup", dataProvider = "emailsProvider")
+    public void selectRowByEmails(String firstName, String email) {
+        Row row = table.getRow(new Cell(2, firstName), new Cell(4, email));
+        CheckBox checkbox = new CheckBox(row);
+        checkbox.click();
+    }
+
+    @DataProvider
+    public static Object[][] emailsProvider() {
+        return new Object[][]{
+                {"Nick", "nickwhite@mail.com"},
+                {"David", "davidmiller@mail.com"},
+                {"Peter", "peterparker@mail.com"}
+        };
     }
 }
