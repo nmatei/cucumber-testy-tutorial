0108f9637b8642f759f5bb935d181cff34c61ba2
Matei Nicolae <matei.nick@gmail.com>
2017-05-12 23:24:14 +0300

    revert changes that we make in class

--- src/test/java/org/fasttrackit/util/TestBase.java
+++ src/test/java/org/fasttrackit/util/TestBase.java
@@ -1,14 +1,10 @@
 package org.fasttrackit.util;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.sdl.selenium.utils.config.WebDriverConfig;
 import com.sdl.selenium.web.Browser;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class TestBase {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestBase.class);
@@ -25,17 +21,5 @@ public abstract class TestBase {
         } catch (Exception e) {
             LOGGER.error("Exception when start suite", e);
         }
-    }
-
-    public void doLogin(String user, String pass) {
-        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
-
-        driver.findElement(By.name("username")).sendKeys(user);
-
-        WebElement passwordElement = driver.findElement(By.id("password"));
-        passwordElement.sendKeys(pass);
-
-        WebElement loginBtn = driver.findElement(By.id("loginButton"));
-        loginBtn.click();
     }
 }
