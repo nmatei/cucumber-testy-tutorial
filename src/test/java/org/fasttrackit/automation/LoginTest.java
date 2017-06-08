494fa7b55facabe412693302cf34d38679173068
Matei Nicolae <matei.nick@gmail.com>
2017-06-08 21:08:48 +0300

    add invalid login with dataProvider

--- src/test/java/org/fasttrackit/automation/LoginTest.java
+++ src/test/java/org/fasttrackit/automation/LoginTest.java
@@ -1,9 +1,9 @@
 package org.fasttrackit.automation;
 
-import com.sdl.selenium.web.utils.Utils;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,14 +19,25 @@ public class LoginTest extends TestBase {
         logoutBtn.click();
     }
 
-    @Test
-    public void invalidUserAndPasswordText() {
-        doLogin("wrong@user", "wrong.pass");
+    @Test(dataProvider = "invalidUsers")
+    public void invalidLoginText(String user, String pass, String expectedMessage) {
+        doLogin(user, pass);
 
         WebElement errorMsg = driver.findElement(By.className("error-msg"));
         String message = errorMsg.getText();
         System.out.println(message);
 
-        assertThat(message, is("Invalid user or password!"));
+        assertThat(message, is(expectedMessage));
     }
+
+    @DataProvider
+    public Object[][] invalidUsers() {
+        return new Object[][]{
+                {"wrong@user", "wrong.pass", "Invalid user or password!"},
+                {"empty.pass@user", "", "Please enter your password!"},
+                {"", "empty.user", "Please enter your email!"},
+                {"", "", "Please enter your email!"}
+        };
+    }
+
 }
