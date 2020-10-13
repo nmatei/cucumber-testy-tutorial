d0b4d83813bb8393cdc0d63a39c2eb5600e08991
Nicolae Matei <nmatei@sdl.com>
2020-10-13 11:08:15 +0300

    use browser.properties for startup configuration

--- src/test/java/org/fasttrackit/util/TestBase.java
+++ src/test/java/org/fasttrackit/util/TestBase.java
@@ -1,7 +1,7 @@
 package org.fasttrackit.util;
 
 import com.sdl.selenium.utils.config.WebDriverConfig;
-import com.sdl.selenium.web.Browser;
+//import com.sdl.selenium.web.Browser;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +17,9 @@ public abstract class TestBase {
 
     private static void startSuite() {
         try {
-            driver = WebDriverConfig.getWebDriver(Browser.CHROME);
+            String browser = "browser.properties";
+            driver = WebDriverConfig.getWebDriver(browser);
+//            driver = WebDriverConfig.getWebDriver(Browser.CHROME);
         } catch (Exception e) {
             LOGGER.error("Exception when start suite", e);
         }
